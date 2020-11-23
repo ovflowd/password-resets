0f5d370e1472e8741a69495e11e6cd5815f96b58
Andrea Veri <averi@redhat.com>
2020-11-23 12:29:00 +0100

    db is now a python module

--- jobs/expired-token.py
+++ jobs/expired-token.py
@@ -6,8 +6,8 @@ import os
 DOCUMENT_ROOT = os.environ.get('DOCUMENT_ROOT')
 sys.path.append(f"{ DOCUMENT_ROOT }/db")
 
-from db import Token
-from base import Session
+from db.db import Token
+from db.base import Session
 from datetime import datetime, timedelta
 
 def expired_token():
@@ -16,7 +16,7 @@ def expired_token():
     for t in tokens:
         if datetime.now() > t.date + timedelta(hours=1):
             t.expired = 1
-            
+
     Session.commit()
     Session.close()
 
